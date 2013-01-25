291131559f82dfde2a55a7647cde8befa22f9fa8
Matt Kneiser <mattman@umich.edu>
2013-01-25 04:53:43 -0500

    Finished the map implementation

--- public/js/map.js
+++ public/js/map.js
@@ -31,63 +31,61 @@ function parseDate(jsonDate, debug) {
 var palette = new Rickshaw.Color.Palette();
 
 new Rickshaw.Graph.JSONP({
-        element: document.querySelector('#chart'),
-        dataURL: 'https://api.github.com/repos/joyent/node/commits',
-        onData: function(d) {
-                var commitDates = [];
-                console.log(d.data);
-                console.log(d);
-                var totCommits = 0;
-                d.data.reverse();
-                d.data.forEach(function(com){
-                        //commitDates.push(com.commit.committer.date)
-                        ++totCommits;
-                        var x = parseDate(com.commit.committer.date, false);
-                        var y = totCommits;
-                        var data_pt = {};
-                        data_pt.x = x;
-                        data_pt.y = y;
-                        console.log(data_pt);
-                        commitDates.push(data_pt);
-                        //console.log(com.commit.committer.date);
-                        //console.log(parseJsonDate(d.data[0].commit.committer.date).valueOf());
-                        //console.log('--------');
-                        //parseDate(com.commit.committer.date)
-                })
+  element: document.querySelector('#chart'),
+  width: 550,
+  height: 250,
+  dataURL: 'https://api.github.com/repos/joyent/node/commits',
+  onData: function(d) {
+    var commitDates = [];
+    console.log(d.data);
+    console.log(d);
+    var totCommits = 0;
 
-                var series = [];
-                var obj = {};
-                obj.name = "Node";
-                obj.data = commitDates;
-                obj.color = palette.color();
+    d.data.reverse();
+    d.data.forEach(function(com){
+      ++totCommits;
+      var x = parseDate(com.commit.committer.date, false);
+      var y = totCommits;
+      var data_pt = {};
+      data_pt.x = x;
+      data_pt.y = y;
+      console.log(data_pt);
+      commitDates.push(data_pt);
+    });
 
-                series.push(obj);
+    var series = [];
+    var obj = {};
+    obj.name = "Node";
+    obj.data = commitDates;
+    obj.color = palette.color();
+    series.push(obj);
+    console.log(series);
 
-                console.log(series);
-                //console.log(commitDates);
+    return series;
+  },
+  onComplete: function(transport) {
+    console.log('transport')
+    console.log(transport)
 
-                //console.log(d);
-                //console.log(d.data[0]);
-                //console.log(d.data[0].commit.committer.date.parseJsonDate());
+    var graph = transport.graph;
+    var detail = new Rickshaw.Graph.HoverDetail({ graph: graph });
 
-                /*console.log(parseJsonDate(d.data[0].commit.committer.date));
-                console.log(typeof(parseJsonDate(d.data[0].commit.committer.date)));
-                console.log(parseJsonDate(d.data[0].commit.committer.date).valueOf());*/
+    var x_axis = new Rickshaw.Graph.Axis.Time( { graph: graph } );
 
-                //console.log(d.data[0].commit.committer.date);
-                //console.log(d.data[0].commit)
-                //console.log(d.data[0].commit.committer)
-                //console.log(d.data[0].commit.committer.date)
-                //d[0].data[0].y += 1;
-                //return d.data[0].commit.committer.date;
-                return series;
-        },
-        onComplete: function(transport) {
-                console.log('transport')
-                console.log(transport)
-                var graph = transport.graph;
-                var detail = new Rickshaw.Graph.HoverDetail({ graph: graph });
-        }
+    var y_axis = new Rickshaw.Graph.Axis.Y( {
+            graph: graph,
+            orientation: 'left',
+            tickFormat: Rickshaw.Fixtures.Number.formatKMBT,
+            element: document.getElementById('y_axis'),
+    } );
+
+    var legend = new Rickshaw.Graph.Legend( {
+            element: document.querySelector('#legend'),
+            graph: graph
+    } );
+
+    //graph.render();
+  }
 });
 
 /*$.get('https://api.github.com/repos/joyent/node/commits', function(d) {
