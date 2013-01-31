1cf643564826b0fbd6c99e9f40bd2faf827b1d22
Matt Kneiser <mattman@umich.edu>
2013-01-31 03:26:38 -0500

    keycode focus

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,11 @@
 $(function() {
 	//console.log('test')
+  $('#password').focus(function(){
+    $('#password').keypress(function(k){
+      console.log(k, 'fired!');
+      if(k.keycode == 13){
+        $.post('/start', { password: $('#password').val() });
+      }
+    });
+  });
 });
