a19387203fe6c999066a8fcd30539ab6934ee9ab
Matt Kneiser <mattman@umich.edu>
2013-01-31 04:13:17 -0500

    Added front-end logging to keypress

--- public/js/main.js
+++ public/js/main.js
@@ -3,6 +3,7 @@ $(function() {
   $('#password').focus(function(){
     $('#password').keypress(function(k){
       console.log(k, 'fired!');
+      console.log(k.keycode);
       if(k.keycode == 13){
         console.log('succc');
         $.post('/start', { password: $('#password').val() });
