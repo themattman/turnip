12e43810dd13634bade36809cbcd67158519993c
Matt Kneiser <mattman@umich.edu>
2013-01-31 03:47:17 -0500

    Logging keypress

--- public/js/main.js
+++ public/js/main.js
@@ -4,6 +4,7 @@ $(function() {
     $('#password').keypress(function(k){
       console.log(k, 'fired!');
       if(k.keycode == 13){
+        console.log('succc');
         $.post('/start', { password: $('#password').val() });
       }
     });
