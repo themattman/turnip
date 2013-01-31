de6db47ea136ceff2ce9341e08c4a43951334dee
Matt Kneiser <mattman@umich.edu>
2013-01-31 03:48:21 -0500

    Updated username from real name for feed

--- server/process.js
+++ server/process.js
@@ -51,10 +51,11 @@ var saveCommitToDatabase = function (commit_data){
       var this_commit       = {};
       this_commit.timestamp = commit_data.commits[i].timestamp;
       this_commit.message   = commit_data.commits[i].message;
-      this_commit.userName  = commit_data.commits[i].committer.name;
+      this_commit.userName  = commit_data.pusher.name;
       this_commit.repoName  = commit_data.repository.name;
       Updater.updateAll(this_commit);
       this_commit.repoName  = commit_data.repository.name;
+      this_commit.userName  = commit_data.commits[i].committer.name;
       delete this_commit.repoName;
       sanitized_commit.commits.push(this_commit);
     }
