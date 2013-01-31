aebf63260e0cf8c82a918b1006e50dc58f3c9c31
Matt Kneiser <mattman@umich.edu>
2013-01-31 05:18:58 -0500

    Autofocus on the access.jade

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,6 @@
 $(function() {
   $('#password').focus(function(){
     $('#password').keypress(function(k){
-      console.log(k, 'fired!');
-      console.log(k.keyCode);
       if(k.keyCode == 13){
         console.log('succc');
         $.post('/start', { password: $('#password').val() });
